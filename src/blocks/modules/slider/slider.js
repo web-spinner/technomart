2236f0b5df3d1bf8757509e71ab0d24f349b172b
Maron <web-spinner@mail.ru>
2021-03-30 15:11:54 +1000

    add scripts for slider tabs

---
+++ src/blocks/modules/slider/slider.js
@@ -0,0 +1,5 @@
+import slick from 'slick-carousel';
+$(document).ready(function(){
+  $('.slider').slick();
+});
+
---
+++ src/blocks/modules/tabs/tabs.js
@@ -0,0 +1,7 @@
+import tabs from "jquery-ui/ui/widgets/tabs";
+
+$(document).ready(function(){
+  $( function() {
+    $( "#tabs"  ).tabs();
+  });
+});
