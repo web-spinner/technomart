e5981e198b8e62d99b7cd39fd5e717ac891c14d8
Maron <web-spinner@mail.ru>
2021-05-03 19:42:55 +1000

    add scripts to range-slider.js

--- src/blocks/modules/range-slider/range-slider.js
+++ src/blocks/modules/range-slider/range-slider.js
@@ -1,12 +1,39 @@
 import noUiSlider from "nouislider/distribute/nouislider.js";
 
-var rangeSlider = document.getElementById('noUiSlider');
+const rangeSlider = document.getElementById('noUiSlider');
+const priceMinInput = document.querySelector('#price-min-input');
+const priceMaxInput = document.querySelector('#price-max-input');
+const priceMinBox = document.querySelector('#price-min-box');
+const priceMaxBox = document.querySelector('#price-max-box');
+const min = +priceMinInput.getAttribute('data-min');
+const max = +priceMaxInput.getAttribute('data-max');
+const startMin = +priceMinInput.getAttribute('data-start');
+const startMax = +priceMaxInput.getAttribute('data-start');
 
 noUiSlider.create(rangeSlider, {
-      start: [20, 80],
+      start: [startMin, startMax],
       connect: true,
   range: {
-            'min': 0,
-            'max': 100
+            'min': [min],
+            'max': [max]
+  },
+  format: {
+    to: function(value){
+      return Math.round(value);
+    },
+    from: function(value){
+      return Number(value);
+    }
   }
 });
+
+rangeSlider.noUiSlider.on('update', function(values){
+  let min = values[0];
+  let max = values[1];
+
+  priceMinBox.textContent = min;
+  priceMaxBox.textContent = max;
+  priceMinInput.value = min;
+  priceMaxInput.value = max;
+
+})
